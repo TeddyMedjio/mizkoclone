ffedece04b031b48f787e2811eb15c8de246c4f7
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-05-07 18:55:03 +0100

    quality

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,12 +4,13 @@ const About = () => {
   return (
     <div className="px-0 md:px-5">
       <div className=" pb-30 border-x border-[#222222] border-b 2xl:w-[70%] 2xl:mx-auto ">
-        <div className="relative h-[300px] lg:h-[920px]">
+        <div className="relative h-[300px] lg:h-[820px]">
           <Image
+            quality={100}
             fill={true}
             src="/background.jpg"
             alt="image of office"
-            className=" object-cover absolute top-0"
+            className=" object-cover absolute"
           />
         </div>
 
--- app/components/Slider.jsx
+++ app/components/Slider.jsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 const Slider = () => {
   return (
     <div className="px-0 md:px-5">
-      <div className=" lg:py-20 border-x border-[#222222] border-b w-full  2xl:w-[70%] 2xl:mx-auto">
+      <div className="lg:pb-10 border-x border-[#222222] border-b w-full  2xl:w-[70%] 2xl:mx-auto">
         <h3 className=" text-base text-center text-grey py-10">
           Proud to have worked with:
         </h3>
         <div className="scroller mb-14 lg:my-0">
-          <div className="scroller__inner flex flex-nowrap gap-10 ">
+          <div className="scroller__inner flex flex-nowrap gap-10">
             <Image
               src="/challenge.svg"
               height={150}
