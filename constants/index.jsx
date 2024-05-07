6455928bfe7c294adad3b4fd1db89e647aafd975
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-05-07 10:58:41 +0100

    favicon

--- constants/index.jsx
+++ constants/index.jsx
@@ -1,10 +1,11 @@
 // NAVIGATION
 export const NAV_LINKS = [
   { href: "/", key: "home", label: "Home" },
-  { href: "#", key: "courses", label: "Courses" },
-  { href: "#", key: "case", label: "Case studies" },
-  { href: "#", key: "testimonials", label: "Testimonials" },
-  { href: "#", key: "services", label: "Services" },
-  { href: "#", key: "partnerships", label: "Partnerships" },
   { href: "#", key: "about ", label: "About " },
+  { href: "#", key: "courses", label: "Projets" },
+  {
+    href: "https://scarfacedigitalagency.vercel.app/",
+    key: "agency",
+    label: "Agency",
+  },
 ];
