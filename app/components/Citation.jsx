fdf11e2db298b17bd0b44b537b591c32ede8fd82
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-05-07 22:13:39 +0100

    quote mark

--- app/components/Citation.jsx
+++ app/components/Citation.jsx
@@ -4,10 +4,10 @@ const Citation = () => {
       <div className="py-20 border-x border-[#222222] border-b 2xl:w-[70%] 2xl:mx-auto">
         <div className="max-w-[900px] mx-auto px-5 lg:px-0 ">
           <h2 className="text-center text-xl md:text-3xl font-heading">
-            " True success lies at the crossroads where design meets business
-            and marketing—it&apos;s a symphony, not a solo. To achieve
+            &quot; True success lies at the crossroads where design meets
+            business and marketing—it&apos;s a symphony, not a solo. To achieve
             greatness, one must learn the art of balancing these core principles
-            for results that resonate beyond the ordinary. "
+            for results that resonate beyond the ordinary. &quot;
           </h2>
         </div>
       </div>
