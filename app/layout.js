731eab8b749b64af1c539b638bf7d35f8b76e9ed
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-05-21 10:03:44 +0100

    update OG

--- app/layout.js
+++ app/layout.js
@@ -15,29 +15,32 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <meta
-        content="UX/UI designer & Frontend dev based in Cameroon"
         property="og:title"
+        content="UX/UI designer & Frontend dev based in Cameroon"
       />
       <meta
+        name="og:description"
         content="Hello there, I'm N. Jean Claude! I went from designer → agency owner → Passionate about the Web and graphic design,I like to collaborate with people who attach importance to aesthetics, who understand the power of simplicity and who know that a good customer experience is essential."
-        name="description"
       />
       <meta
-        content="https://res.cloudinary.com/dqfd5g2fd/image/upload/v1715899357/qvdstxpszkesyfgwlaua.jpg"
         property="og:image"
+        content="https://res.cloudinary.com/dqfd5g2fd/image/upload/v1715899357/qvdstxpszkesyfgwlaua.jpg"
       />
+      <meta property="og:site_name" content="N. Jean Claude" />
+      <meta property="og:locale" content="fr" />
+      <meta property="og:type" content="website" />
       <meta
+        property="twitter:title"
         content="UX/UI designer & Frontend and i am accessible everywhere in
         the world"
-        property="twitter:title"
       />
       <meta
-        content="Hello there, I'm N. Jean Claude! I went from designer → agency owner → Passionate about the Web and graphic design,I like to collaborate with people who attach importance to aesthetics, who understand the power of simplicity and who know that a good customer experience is essential."
         property="twitter:description"
+        content="Hello there, I'm N. Jean Claude! I went from designer → agency owner → Passionate about the Web and graphic design,I like to collaborate with people who attach importance to aesthetics, who understand the power of simplicity and who know that a good customer experience is essential."
       />
       <meta
-        content="https://res.cloudinary.com/dqfd5g2fd/image/upload/v1715899357/qvdstxpszkesyfgwlaua.jpg"
         property="twitter:image"
+        content="https://res.cloudinary.com/dqfd5g2fd/image/upload/v1715899357/qvdstxpszkesyfgwlaua.jpg"
       />
       <meta name="twitter:card" content="summary_large_image" />
       <meta
@@ -45,12 +48,12 @@ export default function RootLayout({ children }) {
         content="Medjio, ux designer, ui designer, web developer, Next.Js, React.js, Front-end, Visual designer, JavaScript, UX.designer, Tailwind CSS, Graphic.designer, freelancer"
       />
 
-      <meta name="twitter:author" content="@nganmedjio_jean" />
-      <meta name="twitter:site" content="@nganmedjio_jean" />
+      <meta name="twitter:author" content="@Nganmedjio_jean" />
+      <meta name="twitter:site" content="@Nganmedjio_jean" />
       <meta property="og:locale" content="en" />
       <meta property="og:site_name" content="Medjio" />
       <meta property="og:type" content="website" />
-      <meta content="summary_large_image" name="twitter:card" />
+
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <body className={`${GeistSans.className} antialiased`}>{children}</body>
     </html>
