b6247a76797e7c1083d245b2efaf68b984d1c79c
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-05-21 17:38:23 +0100

    experience

--- constants/index.jsx
+++ constants/index.jsx
@@ -15,28 +15,28 @@ export const SKILL = [
   {
     key: "uxdesigner",
     title: "UX Designer",
-    year: "10 YEARS",
+    year: "06 YEARS",
     description:
       "10 years of design experience, helping my clients make big money and reach over 300 million users; I merge great aesthetics, design thinking with strategic growth tactics.",
   },
   {
     key: "visualdesigner ",
     title: "Visual Designer ",
-    year: "10 YEARS",
+    year: "07 YEARS",
     description:
       "With a solid track record in designing websites, I deliver strong and user-friendly digital designs. (Since 10 Years in combination with development)",
   },
   {
     key: "graphicdesigner ",
     title: "Graphic Designer ",
-    year: "14 YEARS",
+    year: "+12 YEARS",
     description:
       "I spent most of the last years helping companies scale their business by getting products from 0 to 1 and shaping the brands behind them.",
   },
   {
     key: "development ",
     title: "Development ",
-    year: "08 YEARS",
+    year: "04 YEARS",
     description:
       "I build scalable websites from scratch that fit seamlessly with design. My focus is on micro animations, transitions and interaction.",
   },
