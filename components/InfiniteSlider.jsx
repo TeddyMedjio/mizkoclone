d61100ac356b68660fdd3ad3c69df998f72c2689
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-09-30 20:13:22 +0100

    slider height

--- components/InfiniteSlider.jsx
+++ components/InfiniteSlider.jsx
@@ -13,16 +13,16 @@ export default function InfiniteSlider() {
       blurBoderColor={"#000"}
     >
       <Slider.Slide>
-        <img src="/challenge.svg" alt="any" className="w-36" />
+        <img src="/challenge.svg" alt="any" className="w-32" />
       </Slider.Slide>
       <Slider.Slide>
-        <img src="/marketing.svg" alt="any2" className="w-36" />
+        <img src="/marketing.svg" alt="any2" className="w-32" />
       </Slider.Slide>
       <Slider.Slide>
-        <img src="/laravelshopper.svg" alt="any3" className="w-36" />
+        <img src="/laravelshopper.svg" alt="any3" className="w-40" />
       </Slider.Slide>
       <Slider.Slide>
-        <img src="/zuhe.svg" alt="any3" className="w-36" />
+        <img src="/zuhe.svg" alt="any3" className="w-32" />
       </Slider.Slide>
       {/* <Slider.Slide>
         <img src="/laravelCameroon.svg" alt="any3" className="w-20" />
